7a0e95aad71627cb557e01aecbec78d56450b0c8
25ChilingH <25ChilingH@students.harker.org>
2023-03-05 13:19:54 -0800

    Remove multiprocessing

--- detect.py
+++ detect.py
@@ -232,10 +232,7 @@ def run(
                     print("Not enough streets detected")
         
         if gps:
-            latitude = multiprocessing.Value('d', 1.0)
-            longitude = multiprocessing.Value('d', 1.0)
-            gps_thread = multiprocessing.Process(target = functions.readGPS, args=(latitude, longitude, "{log_path}.txt"))
-            gps_thread.start()
+            functions.readGPS(0.0, 0.0, f"{log_path}.txt")
         # Print time (inference-only)
         LOGGER.info(f'{s}Done. ({t3 - t2:.3f}s)')
 
