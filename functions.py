f061c0e96f270bbcf2800bc0d2da2812ed44ca65
25ChilingH <25ChilingH@students.harker.org>
2023-03-05 14:02:39 -0800

    Add exception handling to geocoding

--- functions.py
+++ functions.py
@@ -3,9 +3,9 @@ import cv2
 from geopy.geocoders import ArcGIS
 import time
 import math
-import pynmea2
-import serial
-from datetime import datetime
+# import pynmea2
+# import serial
+# from datetime import datetime
 
 reader = easyocr.Reader(['en'])
 geolocator = ArcGIS()
@@ -58,75 +58,78 @@ def giveText(imgpred, image):
 def geocodeIntersection(streets, state="California", country="USA", threshold=5):
     if streets[1][1] - streets[0][1] <= threshold:
         param = "{} and {}, {}, {}".format(streets[0][0], streets[1][0], state, country)
-        location = geolocator.geocode(param)
+        try:
+            location = geolocator.geocode(param)
+        except:
+            return "Unable to geocode"
         return (location.latitude, location.longitude)        
     return "No intersection found"
 
-def readGPS(latitude, longitude, path):
-    COM_PORT = "/dev/ttyTHS1"
-    BAUDRATE = 9600
+# def readGPS(latitude, longitude, path):
+#     COM_PORT = "/dev/ttyTHS1"
+#     BAUDRATE = 9600
 
-    print("Running get_lat_lon function")
-    f = open(path, "a")
+#     print("Running get_lat_lon function")
+#     f = open(path, "a")
 
-    serial_port = serial.Serial(COM_PORT, BAUDRATE, timeout = None)
+#     serial_port = serial.Serial(COM_PORT, BAUDRATE, timeout = None)
 
-    # Wait until GPS is released
-    pipe = True
-    while pipe:
-        pipe = serial_port.inWaiting()
-        serial_port.read(pipe)
+#     # Wait until GPS is released
+#     pipe = True
+#     while pipe:
+#         pipe = serial_port.inWaiting()
+#         serial_port.read(pipe)
 
-    try:
-        while True:
-            line = serial_port.readline()
-            line2 = line.decode('latin-1')
+#     try:
+#         while True:
+#             line = serial_port.readline()
+#             line2 = line.decode('latin-1')
 
-            if line2.startswith("$GNGGA"):
-                msg = pynmea2.parse(line2.strip())
+#             if line2.startswith("$GNGGA"):
+#                 msg = pynmea2.parse(line2.strip())
                 
-                latitude.value, longitude.value = msg.latitude, msg.longitude
-                if latitude.value != 0 or longitude.value != 0:
-                    f.write(f"{datetime.now().strftime('%Y:%m:%d:%H:%M:%S')} {msg.timestamp} {str(latitude.value)} {str(longitude.value)} \n")
-                    serial_port.close()
-                    f.flush()
-                    break
-            else:
-                pass
-    except:
-        print("GPS not available")
+#                 latitude.value, longitude.value = msg.latitude, msg.longitude
+#                 if latitude.value != 0 or longitude.value != 0:
+#                     f.write(f"{datetime.now().strftime('%Y:%m:%d:%H:%M:%S')} {msg.timestamp} {str(latitude.value)} {str(longitude.value)} \n")
+#                     serial_port.close()
+#                     f.flush()
+#                     break
+#             else:
+#                 pass
+#     except:
+#         print("GPS not available")
         
-# focal length in mm
-f = 3.04
-# real height of a street sign in mm = 6 inches
-real_height = 152.4
-# image height in pixels
-image_height = 416
-# camera/sensor height in mm
-sensor_height = 23
-def distance(pred):
-    # object height in pixels
-    object_height = pred[3]-pred[1]
-    return (f * real_height * image_height)/(object_height * sensor_height)
+# # focal length in mm
+# f = 3.04
+# # real height of a street sign in mm = 6 inches
+# real_height = 152.4
+# # image height in pixels
+# image_height = 416
+# # camera/sensor height in mm
+# sensor_height = 23
+# def distance(pred):
+#     # object height in pixels
+#     object_height = pred[3]-pred[1]
+#     return (f * real_height * image_height)/(object_height * sensor_height)
 
-def translate_latlong(lat,long,lat_translation_meters,long_translation_meters):
-    ''' method to move any lat,long point by provided meters in lat and long direction.
-    params :
-        lat,long: lattitude and longitude in degrees as decimal values, e.g. 37.43609517497065, -122.17226450150885
-        lat_translation_meters: movement of point in meters in latitude direction.
-                                positive value: up move, negative value: down move
-        long_translation_meters: movement of point in meters in longitude direction.
-                                positive value: left move, negative value: right move
-        '''
-    earth_radius = 6378.137
+# def translate_latlong(lat,long,lat_translation_meters,long_translation_meters):
+#     ''' method to move any lat,long point by provided meters in lat and long direction.
+#     params :
+#         lat,long: lattitude and longitude in degrees as decimal values, e.g. 37.43609517497065, -122.17226450150885
+#         lat_translation_meters: movement of point in meters in latitude direction.
+#                                 positive value: up move, negative value: down move
+#         long_translation_meters: movement of point in meters in longitude direction.
+#                                 positive value: left move, negative value: right move
+#         '''
+#     earth_radius = 6378.137
 
-    #Calculate top, which is lat_translation_meters above
-    m_lat = (1 / ((2 * math.pi / 360) * earth_radius)) / 1000;  
-    lat_new = lat + (lat_translation_meters * m_lat)
+#     #Calculate top, which is lat_translation_meters above
+#     m_lat = (1 / ((2 * math.pi / 360) * earth_radius)) / 1000;  
+#     lat_new = lat + (lat_translation_meters * m_lat)
 
-    #Calculate right, which is long_translation_meters right
-    m_long = (1 / ((2 * math.pi / 360) * earth_radius)) / 1000;  # 1 meter in degree
-    long_new = long + (long_translation_meters * m_long) / math.cos(lat * (math.pi / 180))
+#     #Calculate right, which is long_translation_meters right
+#     m_long = (1 / ((2 * math.pi / 360) * earth_radius)) / 1000;  # 1 meter in degree
+#     long_new = long + (long_translation_meters * m_long) / math.cos(lat * (math.pi / 180))
     
-    return lat_new,long_new
+#     return lat_new,long_new
 
